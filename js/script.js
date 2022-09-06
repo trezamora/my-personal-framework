561319396513665de1f541157dd16ab25c57631f
trezamora <tzamora@hawaii.edu>
2022-09-05 19:03:44 -1000

    site-nav partial

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// OVERLAY NAV MENU DRAWER
+
+const mymenubutton = document.querySelector('.menu-button');
+const mysitenav = document.querySelector('.site-header .site-nav');
+
+mymenubutton.onclick = function () {
+    if (mysitenav.getAttribute('data-navstate') === 'open') {
+        mysitenav.setAttribute('data-navstate', 'closed');
+    } else {
+        mysitenav.setAttribute('data-navstate', 'open');
+    }
+}
