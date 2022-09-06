dc69f33ffc1394ff78033345707996b4268d4232
trezamora <tzamora@hawaii.edu>
2022-09-05 21:15:55 -1000

    created animation partial

--- js/script.js
+++ js/script.js
@@ -10,3 +10,21 @@ mymenubutton.onclick = function () {
         mysitenav.setAttribute('data-navstate', 'open');
     }
 }
+
+// SCROLL TRIGGER ANIMATION
+// CHANGE ACTIVE STATE FOR ALL SECTIONS WITH INTERSECTION OBSERVER
+
+const myobserver = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.setAttribute("data-sectionstate", "active");
+        } else {
+            entry.target.setAttribute("data-sectionstate", "inactive");
+        }
+    });
+});
+
+
+document.querySelectorAll(".animate-on-scroll").forEach((section) => {
+    myobserver.observe(section);
+});
